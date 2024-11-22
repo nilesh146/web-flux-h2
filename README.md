941f66dc7e4690227673258fa429fe5edeea1976
nilesh44 <nileshkumarjaiswal7@gmail.com>
2024-11-22 13:28:57 +0530

    initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # web-flux-h2
+
+Running locally 
+To open H2 console: http://localhost:8086/h2-console/ 
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE products (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2),
+    stock INT
+);
